59a4ab35cd4c943996930612d8e09d2541abcb0d
Bret Miller <bret.miller2010@gmail.com>
2024-12-29 15:34:22 -0600

    clean up

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import prisma from "../lib/db"
 
 const AboutPage = async () => {
 
-  const posts = await prisma.user.findMany();
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <div className="text-center bg-[#e5e7eb] rounded-lg px-10 py-5 shadow-lg">
@@ -11,13 +9,6 @@ const AboutPage = async () => {
        The application is a work in progress that will be updated over time.<br/>
        The home page will display various statistics about the tasks that have been added to the application, the Task Page will display all tasks that have been added to the application, and the Contact page has information to reach out.
       </div>
-      <div>{posts.map((ele) => {
-        return (
-          <div key={ele.id}>
-            <div>{ele.task}</div>
-          </div>
-        );
-      })}</div>
     </div>
   );
 };
