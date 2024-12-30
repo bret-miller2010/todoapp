e5b7369dee0413d20a09b55739be8a9d184ba8f0
Bret Miller <bret.miller2010@gmail.com>
2024-12-30 11:58:22 -0600

    Updated task page layout again

--- src/app/components/TodoBox.tsx
+++ src/app/components/TodoBox.tsx
@@ -30,32 +30,62 @@ export const TodoBox = ({
       : "border-black outline-black";
   };
 
+  const calculateRating = () => {
+    const priority = Number(todo.priority);
+    const complexity = Number(todo.complexity);
+    return priority * complexity;
+  };
+
   return (
     <div
-      className={`flex h-[150px] w-[300px] flex-col items-center justify-around rounded-xl border-2 ${borderColor(todo.completed)} bg-white shadow-xl duration-500 hover:scale-110 hover:outline-double py-2`}
+      className={`flex h-[40px] w-full items-center justify-between rounded-md border-2 ${borderColor(todo.completed)} bg-white text-center shadow-xl duration-300 hover:scale-105 hover:outline-double`}
     >
-      <div>{todo.task}</div>
-      <div className="flex justify-between w-full h-full px-2">
-        <div className="flex flex-col items-center justify-around">
-          <div>{todo.complexity}</div>
-          <div>{todo.priority}</div>
-          <div>{todo.due_date}</div>
-        </div>
-        <div className="h-full flex justify-center items-center ">Checklist</div>
-        <div className="flex h-full space-x-5">
-          <button onClick={handleUpdate}>
-            <CheckIcon completed={todo.completed} />
-          </button>
-          <div className="flex flex-col justify-between">
-            <button onClick={handleRemove}>
-              <TrashIcon />
-            </button>
-            <button value={todo.id} onClick={edit}>
-              <EditIcon />
-            </button>
-          </div>
-        </div>
+      <div className="flex w-1/6 justify-around">
+        <button onClick={handleRemove}>
+          <TrashIcon />
+        </button>
+        <button onClick={handleUpdate}>
+          <CheckIcon completed={todo.completed} />
+        </button>
+      </div>
+      <div className="flex w-2/3 justify-around text-center">
+        <div className="w-1/5">{todo.task}</div>
+        <div className="w-1/5">{todo.due_date}</div>
+        <div className="w-1/5">{todo.priority}</div>
+        <div className="w-1/5">{todo.complexity}</div>
+        <div className="w-1/5">{calculateRating()}</div>
+      </div>
+      <div className="w-1/6">
+        <button value={todo.id} onClick={edit}>
+          <EditIcon />
+        </button>
       </div>
     </div>
+    // <div
+    //   className={`flex h-[35px] w-full flex-col items-center justify-around rounded-xl border-2 ${borderColor(todo.completed)} bg-white shadow-xl duration-500 hover:scale-110 hover:outline-double py-2`}
+    // >
+    //   <div>{todo.task}</div>
+    //   <div className="flex justify-between w-full h-full px-2">
+    //     <div className="flex flex-col items-center justify-around">
+    //       <div>{todo.complexity}</div>
+    //       <div>{todo.priority}</div>
+    //       <div>{todo.due_date}</div>
+    //     </div>
+    //     <div className="h-full flex justify-center items-center ">Checklist</div>
+    //     <div className="flex h-full space-x-5">
+    //       <button onClick={handleUpdate}>
+    //         <CheckIcon completed={todo.completed} />
+    //       </button>
+    //       <div className="flex flex-col justify-between">
+    //         <button onClick={handleRemove}>
+    //           <TrashIcon />
+    //         </button>
+    //         <button value={todo.id} onClick={edit}>
+    //           <EditIcon />
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
