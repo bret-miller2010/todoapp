31a0081d0c2813888c43203e468258fda031c247
Bret Miller <bret.miller2010@gmail.com>
2024-12-30 12:14:19 -0600

    Removed commented out code

--- src/app/components/TodoBox.tsx
+++ src/app/components/TodoBox.tsx
@@ -61,31 +61,5 @@ export const TodoBox = ({
         </button>
       </div>
     </div>
-    // <div
-    //   className={`flex h-[35px] w-full flex-col items-center justify-around rounded-xl border-2 ${borderColor(todo.completed)} bg-white shadow-xl duration-500 hover:scale-110 hover:outline-double py-2`}
-    // >
-    //   <div>{todo.task}</div>
-    //   <div className="flex justify-between w-full h-full px-2">
-    //     <div className="flex flex-col items-center justify-around">
-    //       <div>{todo.complexity}</div>
-    //       <div>{todo.priority}</div>
-    //       <div>{todo.due_date}</div>
-    //     </div>
-    //     <div className="h-full flex justify-center items-center ">Checklist</div>
-    //     <div className="flex h-full space-x-5">
-    //       <button onClick={handleUpdate}>
-    //         <CheckIcon completed={todo.completed} />
-    //       </button>
-    //       <div className="flex flex-col justify-between">
-    //         <button onClick={handleRemove}>
-    //           <TrashIcon />
-    //         </button>
-    //         <button value={todo.id} onClick={edit}>
-    //           <EditIcon />
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
